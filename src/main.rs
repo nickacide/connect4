e335a25baa7a60c1ed4e30a59689d46bf8041757
Nicholas <nickacidegamer@gmail.com>
2022-09-06 11:23:59 +0200

    impl print function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[allow(dead_code)]
+use colored;
+use colored::{Colorize, ColoredString};
 #[derive(Clone, Copy)]
 enum Turn {
     Player,
@@ -25,15 +27,37 @@ impl GameState {
     }
     fn new(turn: &Turn) -> GameState {
         GameState {
-            moves: vec![0, 1, 2, 3, 4, 5, 6],
-            board: [None; 42],
+            moves: vec![35, 36, 37, 38, 39, 40, 41],
+            board: [None; 42], //7x6
             red: *turn,
             yellow: Self::invert(*turn),
             current_turn: *turn,
         }
     }
+    fn apply(&mut self) -> &Self {
+        todo!();
+    }
+    fn print(&self) {
+        let red = format!("O").bright_red().bold();
+        let yellow = format!("O").bright_yellow().bold();
+    let none = format!("").bold();
+        let gui_board: Vec<&ColoredString> = self.board.iter().map(|f| match f {
+            Some(Piece::Red) => &red,
+            Some(Piece::Yellow) => &yellow,
+            None => &none,
+        }).collect();
+        for row in 0..6 {
+            println!("+---+---+---+---+---+---+---+");
+            for col in 0..7 {
+                print!("| {} ", gui_board[row * 6 + col]);
+            }
+            print!("|\n")
+        }
+        println!("+---+---+---+---+---+---+---+")
+    }
 }
 
 fn main() {
-    todo!();
+    let state = GameState::new(&Turn::Computer);
+    state.print();
 }
